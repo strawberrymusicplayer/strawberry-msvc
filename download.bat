e06e6b2f7f1d9cd3888e2763c82b849067e00da0
Jonas Kvinge <jonas@jkvinge.net>
2023-06-05 23:38:32 +0200

    Add patch for qtbase tabbar

--- download.bat
+++ download.bat
@@ -133,6 +133,7 @@ https://raw.githubusercontent.com/strawberrymusicplayer/strawberry-msvc-dependen
 https://raw.githubusercontent.com/strawberrymusicplayer/strawberry-msvc-dependencies/master/patches/twolame.patch
 https://raw.githubusercontent.com/strawberrymusicplayer/strawberry-msvc-dependencies/master/patches/icu-uwp.patch
 https://raw.githubusercontent.com/strawberrymusicplayer/strawberry-msvc-dependencies/master/patches/libpsl-time.patch
+https://raw.githubusercontent.com/strawberrymusicplayer/strawberry-msvc-dependencies/master/patches/qtbase-tabbar.patch
 ) do @(
   if not exist %%~nxx (
     echo Downloading %%~nxx
