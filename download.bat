b5b274222defd29f1adfb57e02b35c355224e92f
Jonas Kvinge <jonas@jkvinge.net>
2023-07-17 23:34:10 +0200

    Update nghttp2

--- download.bat
+++ download.bat
@@ -65,7 +65,7 @@ https://tukaani.org/xz/xz-5.4.3.tar.bz2
 https://github.com/google/brotli/archive/refs/tags/v1.0.9.tar.gz
 https://www.cairographics.org/releases/pixman-0.42.2.tar.gz
 https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.11.4/libxml2-v2.11.4.tar.bz2
-https://github.com/nghttp2/nghttp2/releases/download/v1.54.1/nghttp2-1.54.1.tar.bz2
+https://github.com/nghttp2/nghttp2/releases/download/v1.55.1/nghttp2-1.55.1.tar.bz2
 https://sqlite.org/2023/sqlite-autoconf-3420000.tar.gz
 https://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.gz
 https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz
