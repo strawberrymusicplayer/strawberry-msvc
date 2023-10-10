7fdd3f11cf84c7bbce552ea6844429f89413706e
Jonas Kvinge <jonas@jkvinge.net>
2023-10-11 00:28:11 +0200

    Copy kdsingleapplication

--- build.bat
+++ build.bat
@@ -1697,6 +1697,7 @@ copy /y "%prefix_path%\bin\vorbis.dll" || goto end
 copy /y "%prefix_path%\bin\vorbisfile.dll" || goto end
 copy /y "%prefix_path%\bin\wavpackdll.dll" || goto end
 copy /y "%prefix_path%\bin\zlib*.dll" || goto end
+copy /y "%prefix_path%\bin\kdsingleapplication*.dll" || goto end
 
 copy /y "%PREFIX_PATH%\lib\gio\modules\*.dll" ".\gio-modules\" || goto end
 copy /y "%PREFIX_PATH%\plugins\platforms\qwindows*.dll" ".\platforms\" || goto end
