8c9d8fe958ee83452a68951491f51f99ef4ba6b8
Jonas Kvinge <jonas@jkvinge.net>
2026-01-25 05:45:02 +0100

    StrawberryMSVCBuild: Move proxy-libintl earlier

--- StrawberryMSVCBuild.ps1
+++ StrawberryMSVCBuild.ps1
@@ -433,6 +433,7 @@ function GetPackageUrls {
   param()
   $package_urls = @{
     'pkgconf' = "https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-$pkgconf_version.tar.gz"
+    'proxy-libintl' = "https://github.com/frida/proxy-libintl/archive/refs/tags/$proxy_libintl_version/proxy-libintl-$proxy_libintl_version.tar.gz"
     'getopt-win' = "https://github.com/ludvikjerabek/getopt-win/archive/refs/tags/v$getopt_win_version/getopt-win-$getopt_win_version.tar.gz"
     'zlib' = "https://zlib.net/zlib-$zlib_version.tar.gz"
     'openssl' = "https://github.com/openssl/openssl/releases/download/openssl-$openssl_version/openssl-$openssl_version.tar.gz"
@@ -453,7 +454,6 @@ function GetPackageUrls {
     'orc' = "https://gstreamer.freedesktop.org/src/orc/orc-$orc_version.tar.xz"
     'sqlite' = "https://sqlite.org/2026/sqlite-autoconf-$sqlite_version.tar.gz"
     'libproxy' = "https://github.com/libproxy/libproxy/archive/refs/tags/$libproxy_version/libproxy-$libproxy_version.tar.gz"
-    'proxy-libintl' = "https://github.com/frida/proxy-libintl/archive/refs/tags/$proxy_libintl_version/proxy-libintl-$proxy_libintl_version.tar.gz"
     'glib' = "https://download.gnome.org/sources/glib/2.87/glib-$glib_version.tar.xz"
     'libsoup' = "https://download.gnome.org/sources/libsoup/3.6/libsoup-$libsoup_version.tar.xz"
     'glib-networking' = "https://download.gnome.org/sources/glib-networking/2.80/glib-networking-$glib_networking_version.tar.xz"
@@ -1000,6 +1000,21 @@ function Build-Yasm {
   }
 }
 
+function Build-ProxyIntl {
+  Write-Host "Building proxy-libintl" -ForegroundColor Yellow
+  Push-Location $build_path
+  try {
+    DownloadPackage -package_name "proxy-libintl"
+    ExtractPackage "proxy-libintl-$proxy_libintl_version.tar.gz" -ignore_errors $true
+    Set-Location "proxy-libintl-$proxy_libintl_version"
+    MesonBuild
+    CreatePkgConfigFile -prefix $prefix_path_forward -name "libintl" -description "libintl" -url "https://github.com/frida/proxy-libintl" -version $proxy_libintl_version -libs "-L`${libdir} -lintl" -cflags "-I`${includedir}" -output_file "$prefix_path\lib\pkgconfig\intl.pc"
+  }
+  finally {
+    Pop-Location
+  }
+}
+
 function Build-GetOptWin {
   Write-Host "Building getopt-win" -ForegroundColor Yellow
   Push-Location $build_path
@@ -1485,21 +1500,6 @@ function Build-SQLite {
     Copy-Item "*.dll" "$prefix_path\bin\" -Force
     Copy-Item "*.exe" "$prefix_path\bin\" -Force
     CreatePkgConfigFile -prefix $prefix_path_forward -name "SQLite" -description "SQL database engine" -url "https://www.sqlite.org/" -version $sqlite_version -libs "-L`${libdir} -lsqlite3" -cflags "-I`${includedir}" -output_file "$prefix_path\lib\pkgconfig\sqlite3.pc"
-  }
-  finally {
-    Pop-Location
-  }
-}
-
-function Build-ProxyIntl {
-  Write-Host "Building proxy-libintl" -ForegroundColor Yellow
-  Push-Location $build_path
-  try {
-    DownloadPackage -package_name "proxy-libintl"
-    ExtractPackage "proxy-libintl-$proxy_libintl_version.tar.gz" -ignore_errors $true
-    Set-Location "proxy-libintl-$proxy_libintl_version"
-    MesonBuild
-    CreatePkgConfigFile -prefix $prefix_path_forward -name "libintl" -description "libintl" -url "https://github.com/frida/proxy-libintl" -version $proxy_libintl_version -libs "-L`${libdir} -lintl" -cflags "-I`${includedir}" -output_file "$prefix_path\lib\pkgconfig\intl.pc"
   }
   finally {
     Pop-Location
@@ -2756,6 +2756,7 @@ try {
 
   if (-not (Test-Path "$prefix_path\bin\pkgconf.exe")) { $build_queue += "pkgconf" }
   if (-not (Test-Path "$prefix_path\bin\yasm.exe")) { $build_queue += "yasm" }
+  if (-not (Test-Path "$prefix_path\lib\pkgconfig\intl.pc")) { $build_queue += "proxy-libintl" }
   if (-not (Test-Path "$prefix_path\lib\getopt.lib")) { $build_queue += "getopt-win" }
   if (-not (Test-Path "$prefix_path\lib\pkgconfig\zlib.pc")) { $build_queue += "zlib" }
   if (-not (Test-Path "$prefix_path\lib\pkgconfig\openssl.pc")) { $build_queue += "openssl" }
@@ -2779,7 +2780,6 @@ try {
   if (-not (Test-Path "$prefix_path\lib\pkgconfig\libpsl.pc")) { $build_queue += "libpsl" }
   if (-not (Test-Path "$prefix_path\lib\pkgconfig\orc-0.4.pc")) { $build_queue += "orc" }
   if (-not (Test-Path "$prefix_path\lib\pkgconfig\sqlite3.pc")) { $build_queue += "sqlite" }
-  if (-not (Test-Path "$prefix_path\lib\pkgconfig\libintl.pc")) { $build_queue += "proxy-libintl" }
   if (-not (Test-Path "$prefix_path\lib\pkgconfig\glib-2.0.pc")) { $build_queue += "glib" }
   if (-not (Test-Path "$prefix_path\lib\pkgconfig\libsoup-3.0.pc")) { $build_queue += "libsoup" }
   if (-not (Test-Path "$prefix_path\lib\gio\modules\gioopenssl.lib")) { $build_queue += "glib-networking" }
@@ -2852,6 +2852,7 @@ try {
     switch ($component) {
       "pkgconf" { Build-PkgConf }
       "yasm" { Build-Yasm }
+      "proxy-libintl" { Build-ProxyIntl }
       "getopt-win" { Build-GetOptWin }
       "zlib" { Build-Zlib }
       "openssl" { Build-OpenSSL }
@@ -2875,7 +2876,6 @@ try {
       "libpsl" { Build-LibPSL }
       "orc" { Build-Orc }
       "sqlite" { Build-SQLite }
-      "proxy-libintl" { Build-ProxyIntl }
       "glib" { Build-Glib }
       "libsoup" { Build-LibSoup }
       "glib-networking" { Build-GlibNetworking }
